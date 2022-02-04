56d421148812e677148f3e636011900132c8577c
Yash Sehgal <yashsehgal.work@gmail.com>
2022-02-04 11:28:59 +0000

    Taught: variables and data-types

---
+++ index.js
@@ -0,0 +1,39 @@
+// cpp : int number = 0;
+// python : number = 0
+// java : int number = 0; static int number = 0;
+
+// var, let, const
+
+// var / let
+// const 
+
+let number = 1294;
+var age = 19;
+const name = "yash";
+
+console.log(number);
+console.log(age);
+console.log(name);
+
+// camel-casing: camelCase (first word: lowercase, after that ever word will've the first alphabet in uppercase)
+
+let __$___username124_ = "yash";
+
+console.log(__$___username124_);
+
+// Data types
+// int, long int, double, float, string, undefined... Other user-defined data-types
+// arrays, dictionaries, and many other data-structures
+
+let _integer = 12;
+let _longInt =  9386098378650946;
+let _double = 12.3445478;
+let _float = 12.456;
+let _string = "this is a string";
+let _undefined = undefined;
+
+_undefined = "NOT UNDEFINED ANYMORE";
+
+console.log(_integer, _longInt, _double, _float, _string, _undefined);
+console.log("type of _undefined is: " + typeof(_undefined));
+
